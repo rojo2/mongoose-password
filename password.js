23d49094b752e5bb87c3857c2df480337c779642
Aitor <aitor@rojo2.com>
2016-07-20 01:22:20 +0200

    Initial commit

---
+++ password.js
@@ -0,0 +1,35 @@
+const password = require("rojo2-password");
+
+/**
+ * Plugin that adds to a mongoose Schema
+ * a pbkdf2 password hashing.
+ *
+ * @param {mongoose.Schema} schema
+ * @param {Object} options
+ */
+module.exports = function(schema, options) {
+
+  schema.add({
+    password: {
+      type: String
+    }
+  });
+
+  schema.method("hasPassword", function(passwordToVerify) {
+    return password.verify(passwordToVerify, this.password);
+  });
+
+  schema.pre("save", function(next) {
+    if (this.isModified("password")) {
+      password.hash(this.password).then((hashedPassword) => {
+        this.password = hashedPassword;
+        return next();
+      }).catch((error) => {
+        return next(error);
+      });
+    } else {
+      return next();
+    }
+  });
+
+};
---
+++ test/password.js
@@ -0,0 +1,89 @@
+const {expect} = require("chai");
+const mongoose = require("mongoose");
+const password = require("../password");
+
+describe("Mongoose Password", function() {
+
+  this.timeout(5000);
+
+  before((done) => {
+
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost/mongoose-test", (err) => {
+
+      if (err) {
+        return done(err);
+      }
+
+      const UserSchema = new mongoose.Schema({
+        email: {
+          type: String,
+          unique: true
+        }
+      });
+
+      UserSchema.plugin(password);
+      
+      const User = mongoose.model("user", UserSchema);
+      User.remove().then(() => done());
+
+    });
+
+  });
+
+  it("should use hash the password", (done) => {
+      
+    const User = mongoose.model("user");
+
+    const newUser = new User({
+      email: "example@test.com",
+      password: "pruebas"
+    });
+
+    newUser.save().then((savedUser) => {
+      done();
+    }).catch((err) => done(err));
+
+  });
+  
+  it("should verify that the password is incorrect", (done) => {
+
+    const User = mongoose.model("user");
+    User.findOne({ email: "example@test.com" }).then((user) => {
+      user.hasPassword("pru3b4$").then((verified) => {
+        expect(verified).to.be.equal(false);
+        done();
+      }).catch((err) => done(err));
+    }).catch((err) => done(err));
+
+  });
+
+
+  it("should verify that the password is correct", (done) => {
+
+    const User = mongoose.model("user");
+    User.findOne({ email: "example@test.com" }).then((user) => {
+      user.hasPassword("pruebas").then((verified) => {
+        expect(verified).to.be.equal(true);
+        done();
+      }).catch((err) => done(err));
+    }).catch((err) => done(err));
+
+  });
+
+  it("should modify the password and then rehash it", (done) => {
+
+    const User = mongoose.model("user");
+    User.findOne({ email: "example@test.com" }).then((user) => {
+      user.password = "pru3b4$";
+      user.save().then((savedUser) => {
+        savedUser.hasPassword("pru3b4$").then((verified) => {
+          expect(verified).to.be.equal(true);
+          done();
+        }).catch((err) => done(err));
+      }).catch((err) => done(err));
+    }).catch((err) => done(err));
+
+  });
+
+});
